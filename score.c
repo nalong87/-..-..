c53196bd0bdd97ad181ce74f88e12449e2978574
nalong87 <129143275+nalong87@users.noreply.github.com>
2023-05-15 02:22:54 +0000

    switch문 추가

---
+++ score.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+// main(){
+
+//   int a;
+//   printf("점수를 입력해주세요.");
+//   scanf("%d", &a);
+//   if(a>80)
+//       printf("A군요!!");
+
+//   else if(a>70)
+//       printf("B군요!!");
+
+//   else if(a>60)
+//       printf("C군요!!");
+
+//   else if(a>50)
+//       printf("D군요!!");
+
+//   else if(a>40)
+//       printf("F군요!!");
+
+//   printf("점수 조사 끝!");
+// }
